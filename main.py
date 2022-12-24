aab1c01e11d40124734a9b2be5c2bb2cfdbca611
Mohammad <farid0622@outlook.com>
2022-12-24 10:25:37 +0200

    post basic

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -15,7 +24,7 @@ def get_posts():
     return {"data": "This is first post"}
 
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title:  {payload['title']} content:{payload['content']}"}
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post.dict())
+    return {"new_post": post}
